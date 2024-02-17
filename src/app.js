f6eb5120f9d76e22c3d0e2d28335ec5a67d9c6ca
Karen Rubio <120537042+KarenRubio@users.noreply.github.com>
2024-02-17 04:02:13 +0000

    funcional

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.getElementById("domain").innerHTML = generateDomain();
+};
+//write your code here
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateDomain = () => {
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".io", "us"];
+
+  let domainNames = [];
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++)
+          domainNames.push(
+            pronouns[i] + adjectives[j] + nouns[k] + extensions[l]
+          );
+      }
+    }
+
+    return domainNames;
+  }
 };
